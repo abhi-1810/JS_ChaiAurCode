0e1ab75ebdb7f63100c03f8461c0b52545fe7f57
Abhijeet <87408579+abhi-1810@users.noreply.github.com>
2025-01-21 20:30:18 +0000

    Lec_5

--- Foundation/Lec5_datatypes.js
+++ Foundation/Lec5_datatypes.js
@@ -1,3 +1,27 @@
+"use strict";    // treat all JS code as newer version
 
+// alert (3+3)   // we r using nodejs, not browser
 
-alert (3+3)
+console.log(3 
+    + 
+    3);      // this gives correct output but code readability is very poor
+
+console.log("Abhijeet")
+
+let name = "Abhijeet"
+let age = 22
+let isLoggedIn = false
+
+// Number => 2^53
+// bigint => used in very large no. like trading, stock market, reddit, fb, etc.
+// string => ''/"" (preferred is "")
+// boolean => true/false
+// null => standalone value
+// undefined => variable without a value
+// symbol => used for uniqueness
+
+// object
+
+console.log(typeof "Abhijeet"); // name
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
