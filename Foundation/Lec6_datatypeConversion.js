0f48e368551d9ce839ba43bff73f2231689e4862
Abhijeet <87408579+abhi-1810@users.noreply.github.com>
2025-01-21 20:32:46 +0000

    updated

---
+++ Foundation/Lec6_datatypeConversion.js
@@ -0,0 +1,8 @@
+let score = 33
+
+// first find type of the above. But why to find? 
+// because bhot tym hum backend pe kaam krte h aur 
+// frontend wle value send kiye lkin wo family mai hi tha h fir v leigt
+console.log(typeof score);
+console.log(typeof (score));
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # JS_ChaiAurCode
 A code repo for js series of "Chai aur Code"
+
+### Important source for JS
+* tc39 {In lec. 5}
+* mdn
