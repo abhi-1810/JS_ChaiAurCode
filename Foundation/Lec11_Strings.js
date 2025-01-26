fdb916249c2908563ae12395bb59b38b3e1db228
Abhijeet <87408579+abhi-1810@users.noreply.github.com>
2025-01-26 20:34:39 +0000

    updated

---
+++ Foundation/Lec11_Strings.js
@@ -0,0 +1,11 @@
+// Strings    (26/01/2025)
+
+const name = "abhijeet"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");  // not recommended at all
+
+// So we use backticks(``)
+
+console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);  // modern way. this is known as string interpolation
+
