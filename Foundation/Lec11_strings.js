b4632bd65bd5ea62bd08caf71d4e0841ae5f094f
Abhijeet <87408579+abhi-1810@users.noreply.github.com>
2025-01-27 15:38:53 +0000

    updated

--- Foundation/Lec11_strings.js
+++ Foundation/Lec11_strings.js
@@ -9,3 +9,33 @@ const repoCount = 50
 
 console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);  // modern way. this is known as string interpolation
 
+const gameName = new String('abhijeet')
+
+// console.log(gameName[0]);  // --> a
+// console.log(gameName.__proto__); // --> {}. [ye object dikhne me blank h lkin iske andar bhot saare prototypes]
+// we can see list of prototype in console section of browser 
+// by using example (const gameName = new String('abhijeet'))
+// gameName
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt('5'));
+// console.log(gameName.indexOf('j'));
+
+const newString = gameName.substring(0, 4) // last index is excluded
+console.log(newString); // --> hi
+
+const anotherString = gameName.slice(-6, 4)
+console.log(anotherString);
+
+// substring(): 
+// --> treats -ve indexes as 0
+// --> if start index > end index, it swaps the arguments.
+
+// slice():
+// --> treats -ve indexes as offsets from the end of the string
+// --> returns an empty string if the start index > end index.
+
+
+
+// HW To-Do: Make a list of all the methods of strings and go through with all of them
