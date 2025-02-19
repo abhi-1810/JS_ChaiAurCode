ac4a645044db4af2e2b71263341246902ce42cd0
Abhijeet <87408579+abhi-1810@users.noreply.github.com>
2025-02-19 19:11:37 +0000

    updated

---
+++ Foundation/Lec14_Arrays1.js
@@ -0,0 +1,11 @@
+// Arrays-1        (20/02/2025)
+
+const myArr = [0, 1, 2, 3, 4, 5]
+// --> JS arrays are resizable and can contain a mix of diff. data types (e.g., no, boolean, strings, objects, arrays)
+
+console.log(myArr[0]);
+// console.log(myArr["one"]); // wrong
+
+// Shallow Copy & Deep Copy:
+// A shallow copy means that certain (sub-)values are still connected to the original variable. A deep copy means that all of the values of the new variable are copied and disconnected from the original variable.
+
