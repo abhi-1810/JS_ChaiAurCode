db88b61f66d6b8783d5e3a375530c79aec256d2c
Abhijeet <87408579+abhi-1810@users.noreply.github.com>
2025-02-25 03:42:30 +0000

    updated

--- Foundation/Lec15_Arrays2.js
+++ Foundation/Lec15_Arrays2.js
@@ -1,1 +1,15 @@
-// Lec15_Arrays2.js
+// Lec15_Arrays2.js     (25/02/2025)
+
+const marvelHeroes = ["thor", "Ironman", "spiderman"]
+const dcHeroes = ["superman", "flash", "batman"]
+
+// marvelHeroes.push(dcHeroes)
+
+// console.log(marvelHeroes); // O/P: [ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] 
+// yha pe ye pura [ 'superman', 'flash', 'batman' ] single index element treat hoga
+// console.log(marvelHeroes[3][1]); // O/P: flash
+
+// const allHeroes = marvelHeroes.concat(dcHeroes);
+// console.log(allHeroes); // O/P: [ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+// push() vs concat(): push same array modify krta h aur concat new array me krta h
+
