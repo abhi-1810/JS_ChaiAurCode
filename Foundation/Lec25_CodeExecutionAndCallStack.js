4971f7b92689b2f9c950ec647593fe67c5f7e7bb
Abhijeet <87408579+abhi-1810@users.noreply.github.com>
2025-03-27 19:04:09 +0000

    Lec25-completed

--- Foundation/Lec25_CodeExecutionAndCallStack.js
+++ Foundation/Lec25_CodeExecutionAndCallStack.js
@@ -1,4 +1,6 @@
 // How does javascript execute code + call stack          (27/03/2025)
+// Also in Copy Notes
+
 
 // To run a file, JS runs the program in 2 phases:
 // 1. Memory Creation Phase (Creation Phase)
@@ -11,7 +13,6 @@
 
 
 
-
 // **** JS Execution Context ****
 // 1. Global Execution Context
 // 2. Function Execution Context
@@ -19,29 +20,61 @@
 
 
 
-
-// **** How Code Execute in JS ****
+// **** HOW CODE EXECUTE IN JS ****
 // It will run in 2 phase:
 // 1. Memory Creation Phase (or creation phase)
 // 2. Execution Phase
 
 
+// **** MEMORY CREATION PHASE ****
+// This phase occurs before the actual execution of the code. The JavaScript engine scans the code and sets up memory for variables and functions.
+
+
+// **** EXECUTION PHASE ****
+// In this phase, the JavaScript engine executes the code line by line in the order it appears. In this phase:
+// -> Variables get assigned values.
+// -> Function calls are executed.
+// -> Code runs in the Call Stack.
+
+
+
+// ********* EXAMPLE: ********
+
+let val1 = 10;
+let val2 = 5;
+function addNum(num1, num2) {
+  let total = num1 + num2;
+  return total;
+}
+let result1 = addNum(val1, val2); // 15
+let result2 = addNum(10, 2);    // 12
+// See notes for detailed steps of execution for this code
 
 
 
 
+// ******** CALL STACK ********
+// The Call Stack is a data structure used by the JavaScript engine to keep track of function execution. It follows the LIFO (Last In, First Out) principle, meaning:
 
-// function one() {
-//     console.log("one")
-//     two()
-// }
-// function two() {
-//     console.log("two")
-//     three()
-// }
-// function three() {
-//     console.log("three")
-// }
-// one() 
-// two()
-// three()
+// --> The last function that is called is the first to be executed and removed from the stack.
+// --> The first function that was called is executed last.
+
+
+// Let's take example to understand this:
+function one() {
+    console.log("one")
+    two()
+}
+function two() {
+    console.log("two")
+    three()
+}
+function three() {
+    console.log("three")
+}
+one() 
+two()
+three()
+
+// we can visualise this in browser via following steps:
+// Inspect -> Sources -> Snippet -> New snippet -> Write code -> Run(Ctrl+Enter) -> Script Execution (Ctrl+\) -> See Call Stack
