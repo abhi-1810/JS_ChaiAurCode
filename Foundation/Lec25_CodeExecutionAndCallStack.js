a4b2083fd33d0bd4d2b190f457bde108cce2320b
Abhijeet <87408579+abhi-1810@users.noreply.github.com>
2025-03-27 17:39:23 +0000

    updated

---
+++ Foundation/Lec25_CodeExecutionAndCallStack.js
@@ -0,0 +1,27 @@
+// How does javascript execute code + call stack          (27/03/2025)
+
+// To run a file, JS runs the program in 2 phases:
+// 1. Memory Creation Phase (Creation Phase)
+// 2. Execution Phase
+
+// But before this first thing that happen when we write any code in JS is *Global Execution Context*(GEC). Ye hmesa hi banega hi banega
+// jaha v GEC bnega usko refer kr diya jaata h 'this' varibale se. isi 'this' variable ke andar rkha h jaata h GEC ko
+// Note: browser's GEC is different and node.js GEC is different. In browser, value of 'this' is Window object.
+// JS is a single-threaded language
+
+
+
+
+// **** JS Execution Context ****
+// 1. Global Execution Context
+// 2. Function Execution Context
+// 3. Eval Execution Context (not imp. coz it is a property of Global object, so it's just like Global)
+
+
+
+
+// **** How Code Execute in JS ****
+// It will run in 2 phase:
+// 1. Memory Creation Phase (or creation phase)
+// 2. Execution Phase
+
