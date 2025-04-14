625c517abd92c4f9ee0316fba55e06c733afb134
Abhijeet <87408579+abhi-1810@users.noreply.github.com>
2025-04-14 08:45:51 +0000

    updated

---
+++ Foundation/Lec30_FilterMapAndReduce.js
@@ -0,0 +1,12 @@
+// Filter map and reduce in javascript          (14/04/2025)
+
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+coding.forEach( item => {
+    // console.log(item);
+})
+
+const values = coding.forEach( item => {
+    console.log(item);
+})
+console.log(values);
