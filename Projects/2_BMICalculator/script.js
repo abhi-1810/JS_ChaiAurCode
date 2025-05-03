065744aa6dbc79bd084a6133c0ae50e86e581038
Abhijeet <87408579+abhi-1810@users.noreply.github.com>
2025-05-03 20:25:06 +0000

    Project-2 completed

--- Projects/2_BMICalculator/script.js
+++ Projects/2_BMICalculator/script.js
@@ -4,20 +4,20 @@ const form = document.querySelector('form');
 // const height = parseInt(document.querySelector('#height').value)
 
 form.addEventListener('submit', function (e) {
-  e.preventDefault();
+  e.preventDefault(); // 
 
-  const height = parseFloat(document.querySelector('#height').value);
+  const height = document.querySelector('#height').value;
+  const weight = document.querySelector('#weight').value;
   const validHeight = document.querySelector('#validHeight');
-  const weight = parseFloat(document.querySelector('#weight').value);
   const validWeight = document.querySelector('#validWeight');
   const results = document.querySelector('#results');
 
-  if (height === '' || height < 0 || isNaN(height)) {
+  if (height === '' || parseInt(height) < 0 || isNaN(parseInt(height))) {
     validHeight.innerHTML = `Please give a valid height ${height}`;
-  } else if (weight === '' || weight < 0 || isNaN(weight)) {
+  } else if (weight === '' || parseInt(weight) < 0 || isNaN(parseInt(weight))) {
     validWeight.innerHTML = `Please give a valid weight ${weight}`;
   } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-    results.innerHTML = `<span>${bmi}</span>`;
+    results.innerHTML = `Your BMI is: ${bmi}`;
   }
 });
