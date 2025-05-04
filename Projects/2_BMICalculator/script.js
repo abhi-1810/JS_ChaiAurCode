ba40b5f49c4557f353fdb8c5d64d500a401ffe5e
Abhijeet <87408579+abhi-1810@users.noreply.github.com>
2025-05-04 16:50:09 +0000

    Project-2 completed

--- Projects/2_BMICalculator/script.js
+++ Projects/2_BMICalculator/script.js
@@ -4,17 +4,22 @@ const form = document.querySelector('form');
 // const height = parseInt(document.querySelector('#height').value)
 
 form.addEventListener('submit', function (e) {
-  e.preventDefault(); // 
+  e.preventDefault(); // Prevent default form submission behavior
 
-  const height = document.querySelector('#height').value;
-  const weight = document.querySelector('#weight').value;
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
   const validHeight = document.querySelector('#validHeight');
   const validWeight = document.querySelector('#validWeight');
   const results = document.querySelector('#results');
 
-  if (height === '' || parseInt(height) < 0 || isNaN(parseInt(height))) {
+  // Clear any previous messages
+  validHeight.innerHTML = '';
+  validWeight.innerHTML = '';
+  results.innerHTML = '';
+
+  if (height === '' || height < 0 || isNaN(height)) {
     validHeight.innerHTML = `Please give a valid height ${height}`;
-  } else if (weight === '' || parseInt(weight) < 0 || isNaN(parseInt(weight))) {
+  } else if (weight === '' || weight < 0 || isNaN(weight)) {
     validWeight.innerHTML = `Please give a valid weight ${weight}`;
   } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
