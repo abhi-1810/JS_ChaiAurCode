e515efe74b7e61cda6acf9765247779b6317f931
Abhijeet <87408579+abhi-1810@users.noreply.github.com>
2025-05-05 14:27:13 +0000

    Project-3 completed

--- Projects/3_digitalClock/script.js
+++ Projects/3_digitalClock/script.js
@@ -1,9 +1,16 @@
 const clock = document.getElementById("clock");
-// const clock = document.querySelector('#clock')
+// const clock = document.querySelector('#clock') // 2nd way
 
-// Set Interval
+
+// let date = new Date();
+// console.log(date.toLocaleTimeString());
+
+// isse console me time aane lgega but hume chahiye ki ye har second automatic change ho, iske liye hum use krenge Set Interval ka
+
+
+// Set Interval: it requires a method and a interval (e.g., 1sec.) by which we want to run our program
+// Syntax: setInterval(function() {}, 1000)
 setInterval(function () {
   let date = new Date();
-  // console.log(date.toLocaleTimeString());
   clock.innerHTML = date.toLocaleTimeString();
 }, 1000);
